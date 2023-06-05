abf8433686867a9c075c2261edd018147067391a
germanpazm <german.paz@educa.madrid.org>
2023-06-05 02:15:27 +0200

    first commit

--- src/components/inicio.vue
+++ src/components/inicio.vue
@@ -39,7 +39,7 @@ import { RouterLink } from 'vue-router';
                             <img class="position-absolute w-100 h-100"  src="../assets/pokemon25.jpeg" style="object-fit: cover;">
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style="max-width: 700px;">
-                                    <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown" >Visualiza Tus Pokemon</h1>
+                                    <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown" >Visualiza Tus Pokemons</h1>
                                     <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
                                     <RouterLink class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" to="/comprados">Tus Pokemons</RouterLink>
 
